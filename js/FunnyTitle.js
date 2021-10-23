81f83b1c1ded89f3be76c0f08d97da40eef0b453
CCCSL05 <cl19805120213@163.com>
2021-10-23 18:37:35 +0800

    Site updated: 2021-10-23 18:37:32

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,15 +4,15 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '我相信你还会回来的！';
+        document.title = '我在这儿等着你回来';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/medias/logo.png");
         // document.title = '哈哈，我就知道！' + OriginTitle;
-        document.title = '哈哈，我就知道！' ;
+        document.title = '等着你回来看那桃花开' ;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
-        }, 1000);
+        }, 2000);
     }
 });
